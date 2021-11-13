5cc0d43313ecd77b0c764ef7d768fa097ca81672
gianvittorio <gianvittorio.castellano@gmail.com>
2021-11-13 17:43:52 -0300

    done with postgresql driver

--- hydra/databasehandlers/postgreshandler/postgreshandler.go
+++ hydra/databasehandlers/postgreshandler/postgreshandler.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"strings"
 
 	_ "github.com/lib/pq"
 )
@@ -21,13 +22,70 @@ func main() {
 	connStr := "user=test password=test dbname=test sslmode=disable port=5432"
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Could not connect, error ", err.Error())
 	}
 	defer db.Close()
 
-	rows, err := db.Query("select * from personnel as p limit 10")
+	cw := GetCrewByPositions(db, []string{"'Mechanic'", "'Technician'"})
+	fmt.Println(cw)
+
+	fmt.Println(GetCrewMemberById(db, 4))
+
+	AddCrewMember(db, crewMember{name: "Steve Bee", secClearance: 4, position: "Biologist"})
+
+	// fmt.Println(GetCrewMemberByPosition(db, "Chemist"))
+}
+
+func GetCrewMemberById(db *sql.DB, id int) (cm crewMember) {
+	row := db.QueryRow("select * from personnel where id = $1", id)
+
+	err := row.Scan(&cm.id, &cm.name, &cm.secClearance, &cm.position)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(rows)
+
+	return
+}
+
+func AddCrewMember(db *sql.DB, cm crewMember) int64 {
+
+	res, err := db.Exec("insert into personnel (name, security_clearance, position) values($1, $2, $3)", cm.name, cm.secClearance, cm.position)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ra, _ := res.RowsAffected()
+	id, _ := res.LastInsertId()
+
+	log.Println("Rows Affected", ra, "Last inserted id", id)
+
+	return id
+}
+
+func GetCrewByPositions(db *sql.DB, positions []string) Crew {
+	Qs := fmt.Sprintf("select id, name, security_clearance, position from personnel as p where position in (%s)", strings.Join(positions, ","))
+
+	rows, err := db.Query(Qs)
+	if err != nil {
+		log.Fatal("Could not get data from the personnel table ", err)
+	}
+	defer rows.Close()
+
+	retVal := Crew{}
+	cols, _ := rows.Columns()
+	fmt.Println("Columns detected: ", cols)
+
+	for rows.Next() {
+		member := crewMember{}
+		err = rows.Scan(&member.id, &member.name, &member.secClearance, &member.position)
+		if err != nil {
+			log.Fatal("Error scanning row ", err)
+		}
+		retVal = append(retVal, member)
+	}
+
+	if err := rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return retVal
 }
--- hydra/set_up.sh
+++ hydra/set_up.sh
@@ -10,10 +10,19 @@ docker-compose up -d --build
 sleep 10
 
 docker-compose exec -T hydra-postgresql-db psql -d ${POSTGRES_DB} -U ${POSTGRES_USER} -p ${POSTGRES_PORT} << EOF
+    create sequence if not exists personnel_id_seq start 1;
+    
     create table if not exists personnel (
-        id serial primary key, 
+        id int not null default nextval('personnel_id_seq')::regclass, 
         name varchar(45) not null,
         security_clearance int not null,
-        position varchar(45) not null
-    )
+        position varchar(45) not null,
+        primary key (id)
+    );
+
+    insert into personnel (name, security_clearance, position) values('Isis Adcox', 5, 'Engineer I');
+    insert into personnel (name, security_clearance, position) values('You Chaloux', 8, 'Engineer II');
+    insert into personnel (name, security_clearance, position) values('Lorette Gee', 2, 'Assistant Pilot');
+    insert into personnel (name, security_clearance, position) values('Telma Rosas', 5, 'Mechanic');
+    insert into personnel (name, security_clearance, position) values('Lynsey Christman', 3, 'Technician');
 EOF
